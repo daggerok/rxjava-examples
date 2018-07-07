e1b4ce44f14d9037e4cb601ffe7aa6bca4ff2177
Maksim Kostromin <daggerok@gmail.com>
2018-07-08 00:24:21 +0300

    Who Let the Bugs Out?? (C) whatthecommit.com

--- reactivex-api/src/main/java/daggerok/App.java
+++ reactivex-api/src/main/java/daggerok/App.java
@@ -1,10 +1,12 @@
 package daggerok;
 
+import io.reactivex.Flowable;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
   public static void main(String[] args) {
-    log.info("yo!");
+    Flowable.just("ololo", "trololo")
+            .subscribe(log::info);
   }
 }
